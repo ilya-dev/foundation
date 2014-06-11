b321fc83e724d6beec0e60edb08cb91d3b481458
Ilya S <public.ilya.s@gmail.com>
2014-06-11 23:47:36 +0700

    write some pseudo-code

--- source/Foundation/Base.php
+++ source/Foundation/Base.php
@@ -1,3 +1,21 @@
 <?php namespace Foundation;
 
-class Base {}
+use Block\Block, Creator\Creator;
+
+class Base {
+
+    public function __construct()
+    {
+        $arguments = func_get_args();
+
+        // ... set properties
+
+        if (method_exists($this, 'init'))
+        {
+            call_user_func_array(
+                [$this, 'init'], array_slice($arguments, $someIndex)
+            );
+        }
+    }
+
+}
