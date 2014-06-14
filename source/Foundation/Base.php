352157b28a5a5c92931ed0757d08a0c3405bf098
Ilya S <public.ilya.s@gmail.com>
2014-06-14 23:58:51 +0700

    remove unnecessary variable

--- source/Foundation/Base.php
+++ source/Foundation/Base.php
@@ -8,14 +8,9 @@ class Base {
 
     public function __construct()
     {
-        $arguments = func_get_args();
-
-        // set properties...
-        var_dump($this->__getDependencies());exit;
-
         if (method_exists($this, 'init'))
         {
-            call_user_func_array([$this, 'init'], $arguments);
+            call_user_func_array([$this, 'init'], func_get_args());
         }
     }
 
